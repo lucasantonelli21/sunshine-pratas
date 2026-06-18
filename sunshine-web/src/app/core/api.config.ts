4c106f448ea985f57eaf7ea8694035579bb2ed61
Lucas <lucaslba2105@gmail.com>
2026-06-18 00:47:02 -0300

    mudando url da api

--- sunshine-web/src/app/core/api.config.ts
+++ sunshine-web/src/app/core/api.config.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from '@angular/core';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL', {
   providedIn: 'root',
-  factory: () => 'http://localhost:8080/api',
+  factory: () => 'https://sunshine.aviariocampoalegre.app/api',
 });
